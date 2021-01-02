e114846086bd339ecf82ba3b77a87666fc092ba1
srbsingh3 <32899793+srbsingh3@users.noreply.github.com>
2021-01-02 13:34:05 +0530

    added toast message

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
-const allFrames = figma.currentPage.findAll(node => node.type === "FRAME");
 const mypage = figma.currentPage;
+const allFrames = figma.currentPage.findAll(node => node.type === "FRAME");
 // Horizontal Sorting
 var ordered = allFrames.sort(function (a, b) {
     var compareY = a.y - b.y;
@@ -10,8 +10,8 @@ var ordered = allFrames.sort(function (a, b) {
         return a.x - b.x;
     }
 });
-// Pushing the frames one by one into the parent page
+// Pushing the sorted frames one by one into the parent page
 for (var i = ordered.length - 1; i >= 0; i--) {
     mypage.appendChild(allFrames[i]);
 }
-figma.closePlugin();
+figma.closePlugin('Frames in your layer-list have been arranged horizontally.');
--- code.ts
+++ code.ts
@@ -1,6 +1,8 @@
+const mypage = figma.currentPage
+
 const allFrames = figma.currentPage.findAll(node => node.type === "FRAME")
 
-const mypage = figma.currentPage
+
 
 // Horizontal Sorting
 var ordered = allFrames.sort(function(a,b) {
@@ -12,9 +14,9 @@ var ordered = allFrames.sort(function(a,b) {
   }
 });
 
-// Pushing the frames one by one into the parent page
+// Pushing the sorted frames one by one into the parent page
 for (var i = ordered.length - 1; i >= 0; i--) {
   mypage.appendChild(allFrames[i]);
 }
 
-figma.closePlugin();
+figma.closePlugin('Frames in your layer-list have been arranged horizontally.');
