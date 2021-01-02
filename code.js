9c90c37060ce4f86430937af8bdf0189c035c081
srbsingh3 <32899793+srbsingh3@users.noreply.github.com>
2021-01-02 13:12:53 +0530

    built basic functionality

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
-throw new Error("This plugin template uses Typescript. Follow the instructions in `README.md` to generate `code.js`.")
+const allFrames = figma.currentPage.findAll(node => node.type === "FRAME");
+const mypage = figma.currentPage;
+// Horizontal Sorting
+var ordered = allFrames.sort(function (a, b) {
+    var compareY = a.y - b.y;
+    if (compareY != 0) {
+        return compareY;
+    }
+    else {
+        return a.x - b.x;
+    }
+});
+// Pushing the frames one by one into the parent page
+for (var i = ordered.length - 1; i >= 0; i--) {
+    mypage.appendChild(allFrames[i]);
+}
+figma.closePlugin();
--- code.ts
+++ code.ts
@@ -1,21 +1,20 @@
-// This plugin creates 5 rectangles on the screen.
-const numberOfRectangles = 5
+const allFrames = figma.currentPage.findAll(node => node.type === "FRAME")
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
+const mypage = figma.currentPage
 
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
+// Horizontal Sorting
+var ordered = allFrames.sort(function(a,b) {
+  var compareY = a.y - b.y;
+  if (compareY != 0) {
+    return compareY;
+  } else {
+    return a.x - b.x;
+  }
+});
+
+// Pushing the frames one by one into the parent page
+for (var i = ordered.length - 1; i >= 0; i--) {
+  mypage.appendChild(allFrames[i]);
 }
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
 
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
 figma.closePlugin();
