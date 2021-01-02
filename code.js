672663035edc8c6f369e619a10b6b26b4148ba78
srbsingh3 <32899793+srbsingh3@users.noreply.github.com>
2021-01-02 20:52:43 +0530

    horizontal sorting added

--- code.js
+++ code.js
@@ -1,7 +1,19 @@
 const mypage = figma.currentPage;
 const allFrames = figma.currentPage.findAll(node => node.type === "FRAME");
-// Horizontal Sorting
-var ordered = allFrames.sort(function (a, b) {
+// Filter out frames which are nested inside other frames
+const filteredFrames = allFrames.filter(node => node.parent.type === "PAGE");
+// Virtually align frames
+var myFunction = function (givenArtboard) {
+    for (i = 0; i < filteredFrames.length; i++) {
+        if ((Math.abs(givenArtboard.y - filteredFrames[i].y) < givenArtboard.height) &&
+            (Math.abs(givenArtboard.y - filteredFrames[i].y) < filteredFrames[i].height)) {
+            filteredFrames[i].y = givenArtboard.y;
+        }
+    }
+};
+filteredFrames.forEach(myFunction);
+// Horizontal Sorting of filtered frames
+var ordered = filteredFrames.sort(function (a, b) {
     var compareY = a.y - b.y;
     if (compareY != 0) {
         return compareY;
@@ -12,6 +24,7 @@ var ordered = allFrames.sort(function (a, b) {
 });
 // Pushing the sorted frames one by one into the parent page
 for (var i = ordered.length - 1; i >= 0; i--) {
-    mypage.appendChild(allFrames[i]);
+    mypage.appendChild(filteredFrames[i]);
 }
+// Close plugin with a toast message
 figma.closePlugin('Frames in your layer-list have been arranged horizontally.');
--- code.ts
+++ code.ts
@@ -2,10 +2,25 @@ const mypage = figma.currentPage
 
 const allFrames = figma.currentPage.findAll(node => node.type === "FRAME")
 
+// Filter out frames which are nested inside other frames
+const filteredFrames = allFrames.filter(node => node.parent.type === "PAGE")
 
+// Virtually align frames
+var myFunction = function(givenArtboard) {
+  for (i = 0; i < filteredFrames.length; i++) {
+    if (
+      (Math.abs(givenArtboard.y - filteredFrames[i].y) < givenArtboard.height) &&
+      (Math.abs(givenArtboard.y - filteredFrames[i].y) < filteredFrames[i].height)
+    ) {
+      filteredFrames[i].y = givenArtboard.y;
+    }
+  }
+}
 
-// Horizontal Sorting
-var ordered = allFrames.sort(function(a,b) {
+filteredFrames.forEach(myFunction);
+
+// Horizontal Sorting of filtered frames
+var ordered = filteredFrames.sort(function(a,b) {
   var compareY = a.y - b.y;
   if (compareY != 0) {
     return compareY;
@@ -16,7 +31,8 @@ var ordered = allFrames.sort(function(a,b) {
 
 // Pushing the sorted frames one by one into the parent page
 for (var i = ordered.length - 1; i >= 0; i--) {
-  mypage.appendChild(allFrames[i]);
+  mypage.appendChild(filteredFrames[i]);
 }
 
+// Close plugin with a toast message
 figma.closePlugin('Frames in your layer-list have been arranged horizontally.');
